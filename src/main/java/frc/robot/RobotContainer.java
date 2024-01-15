aef64df759d3203be02291f2fec2a091da24218a
nathansmith117 <114111495+Unicornflart@users.noreply.github.com>
2024-01-15 12:51:06 -0700

    Update RobotContainer.java

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -19,8 +19,7 @@ public class RobotContainer {
   // The robot's subsystems and commands are defined here...
 
   // Replace with CommandPS4Controller or CommandJoystick if needed
-  private final CommandXboxController m_driverController =
-      new CommandXboxController(OperatorConstants.kDriverControllerPort);
+  private final CommandXboxController m_driverController = new CommandXboxController(OperatorConstants.kDriverControllerPort);
 
   /** The container for the robot. Contains subsystems, OI devices, and commands. */
   public RobotContainer() {
@@ -49,7 +48,8 @@ public class RobotContainer {
    *
    * @return the command to run in autonomous
    */
-  public void getAutonomousCommand() { //change return type to Command later, set to void to get rid of error
+  public Command getAutonomousCommand() {
     // An example command will be run in autonomous
+    return null;
   }
 }
