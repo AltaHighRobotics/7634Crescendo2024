2c7244073e3318a0c933a6e8942bc7afa44b0137
Tiny0pus <143680478+Tiny0pus@users.noreply.github.com>
2024-03-14 08:42:58 -0600

    Two Note Auto

--- src/main/java/frc/robot/commands/SillyAuto.java
+++ src/main/java/frc/robot/commands/SillyAuto.java
@@ -18,10 +18,15 @@ public class SillyAuto extends Command {
   private boolean done;
   private boolean shot;
   private boolean outOfZone;
+  private boolean grabbedNote;
   private long startTime;
   private AprilTagSubsystem m_aprilTagSubsystem;
   private ShootSubsystem m_shootSubsystem;
   private int speakerPosition;
+  private double rotationPID;
+  private double xPID;
+  private double yPID;
+  private double startingYdistance;
   public SillyAuto(DriveTrainSub driveTrainSub, AprilTagSubsystem aprilTagSubsystem, ShootSubsystem shootSubsystem) {
     m_driveTrainSub = driveTrainSub;
     m_aprilTagSubsystem = aprilTagSubsystem;
@@ -33,9 +38,16 @@ public class SillyAuto extends Command {
   // Called when the command is initially scheduled.
   @Override
   public void initialize() {
+    startingYdistance = Constants.Y_DIS_SPEAKER;
+    rotationPID = 0;
+    xPID = 0;
+    yPID = 0;
     outOfZone = false;
     done = false;
-    shot = false;
+    firstShot = false;
+    secondShot = false;
+    grabbedNote = false;
+    
     speakerPosition = 10;
     m_driveTrainSub.resetGyro();
     startTime = System.currentTimeMillis();
@@ -47,15 +59,17 @@ public class SillyAuto extends Command {
     PhotonTrackedTarget currentTarget = m_aprilTagSubsystem.getBestTarget();
     //if you dont see a tag and are running out of a time, just move foward. You should get out of the zone.
     if(currentTarget == null && System.currentTimeMillis() - startTime >=10 && !outOfZone){
-      m_driveTrainSub.drive(0,0.5,0,false,1);
+      yPID = 0.5;
     }
-    if(!shot){
+    if(!firstShot){
       m_shootSubsystem.shootSpeaker();
       if(System.currentTimeMillis() - startTime >=2){
         m_shootSubsystem.spinIntakeMotor(Constants.INTAKE_SPEED);
       }
       if(System.currentTimeMillis() - startTime >= 3){
-        shot = true;
+        
+        firstShot = true;
+        startingYdistance = m_aprilTagSubsystem.yDistance(currentTarget);
       }
     }
     //zero your field centric for driving based on april tag
@@ -63,12 +77,32 @@ public class SillyAuto extends Command {
     else if(currentTarget.getFiducialId() == 7 || currentTarget.getFiducialId() == 4){
     speakerPosition = m_aprilTagSubsystem.getSpeakerPosition(currentTarget);
     double startingAngle = m_aprilTagSubsystem.getAngleDifference(currentTarget);
+      double relativeYaw = currentTarget.getYaw();
     m_driveTrainSub.fieldCentricOffset = 180 - (startingAngle * speakerPosition);
+    double yDistance = m_aprilTagSubsystem.yDistance(currentTarget);
+    double xDistance = m_aprilTagSubsystem.xDistance(currentTarget);
     switch(speakerPosition){
       case 1:
         //insert code if youre nearest to the "bad" april tag
         break;
       case 0:
+        //allign ourselves with the april tag
+        xPID = m_aprilTagSubystem.xPID(xDistance, 0);
+        rotationPID = m_aprilTagSubsystem.rotationPID(yaw)
+        //if you havent backed up yet, back up and grab note
+        if(yDistance > 115){
+          grabbedNote = true; 
+        }
+        //if you havent got a note, back up and grab one
+        if (!grabbedNote){
+        m_shootSubsystem.spinIntakeMotor(Constants.INTAKE_SPEED);
+        yPID = m_aprilTagSubsystem.yPID(yDistance, 120);
+        }
+        else {
+        yPID = m_aprilTagSubsyem.yPID(yDistance, startingYdistance);
+        }
+        
+        
         //insert code if youre head on with speaker
         break;
       case -1:
@@ -76,6 +110,7 @@ public class SillyAuto extends Command {
         break;
     }  
     }
+    m_driveTrainSub.drive(xPID,yPID,rotationPID,false,1);
     m_driveTrainSub.run();
     
 
